b59b7226ba688267f2940b9b825890bb38e04e62
Yusuke Kamiyamane <y@y14e.com>
2025-05-31 13:52:22 +0900

    20250531

--- menu-floating-ui.ts
+++ menu-floating-ui.ts
@@ -42,7 +42,7 @@ export class Menu {
       selector: {
         button: '[data-menu-button]',
         list: '[role="menu"]',
-        item: '[role="menuitem"]',
+        item: '[role^="menuitem"]',
       },
       animation: {
         duration: 300,
