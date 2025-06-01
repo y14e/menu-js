57062e0849231880e9b808531e82a78fa9d56bf0
Yusuke Kamiyamane <y@y14e.com>
2025-06-02 01:15:09 +0900

    20250602

--- menu.ts
+++ menu.ts
@@ -408,13 +408,15 @@ export class Menu {
   }
 
   close(): void {
+    if (this.submenus.length) {
+      this.submenus.forEach(menu => {
+        menu.close();
+      });
+    }
     if (!this.buttonElement || this.buttonElement.getAttribute('aria-expanded') !== 'true') {
       return;
     }
     this.toggle(false);
-    this.submenus.forEach(menu => {
-      menu.close();
-    });
     if (this.buttonElement && this.rootElement.contains(document.activeElement)) {
       this.buttonElement.focus();
     }
