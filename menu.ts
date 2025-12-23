92f36a6c419ee15f2755cb4faa1a573333251c7f
Yusuke Kamiyamane <y@y14e.com>
2025-12-23 19:07:03 +0900

    20251223

--- menu.ts
+++ menu.ts
@@ -389,8 +389,7 @@ export default class Menu {
         const foundIndex = targetFocusables.findIndex(focusable => focusables.indexOf(focusable) > currentIndex);
         newIndex = foundIndex !== -1 ? foundIndex : 0;
     }
-    const focusable = targetFocusables[newIndex];
-    focusable.focus();
+    targetFocusables[newIndex].focus();
   }
 
   private handleItemBlur(event: FocusEvent): void {
